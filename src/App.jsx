43ae0dbddd1297e5f89179fd5d32e1ef0bd149a8
rahulb-voltreum <rahul.banerjee@voltreum.com>
2025-08-07 11:45:50 +0530

    App changes new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navigation from "./components/Navigation";
 import Home from "./sections/Home";
-import MyWork from "./sections/Mywork";
+import MyWork from "./sections/MyWork";
 import About from "./sections/About";
 import Skills from "./sections/Skills";
 import Contact from "./sections/Contact";
