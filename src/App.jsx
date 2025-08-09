c97dc349c52670183e96ea21c81a78b9c8e7becb
rahulb-voltreum <rahul.banerjee@voltreum.com>
2025-08-09 18:04:22 +0530

    Removed loading bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-import React, { useRef, useEffect, useState } from "react";
 import Navigation from "./components/Navigation";
 import Home from "./sections/Home";
 import MyWork from "./sections/MyWork";
 import About from "./sections/About";
 import Skills from "./sections/Skills";
 import Contact from "./sections/Contact";
-import LoadingBar from "react-top-loading-bar";
 
 
 function App() {
 
- const loadingBarRef = useRef(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    loadingBarRef.current.continuousStart();
-
-    window.addEventListener("load", () => {
-      loadingBarRef.current.complete();
-      setTimeout(() => setIsLoaded(true), 500);
-    });
-  }, []);
-
-  if (!isLoaded) {
-    return <LoadingBar ref={loadingBarRef} color="#ff4400ff" />;
-  }
-
-
+ 
   return (
     <>
       <Navigation />
